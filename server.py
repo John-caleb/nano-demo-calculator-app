e2d162cbec30bc094c41986e2aefd2cef8448c95
John Caleb <calebgarikipati@gmail.com>
2023-09-14 18:02:54 +0530

    added multiply

--- server.py
+++ server.py
@@ -7,15 +7,24 @@ app = Flask(__name__)
 def greeting():
     return 'Hello World!'
 
+
 @app.route("/calculator/add", methods=['POST'])
 def add():
     numbers = request.json
     return jsonify(int(numbers['first'] + numbers['second']))
+
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
     numbers = request.json
     return jsonify(int(numbers['first'] - numbers['second']))
 
+
+@app.route("/calculator/multiply", methods=['POST'])
+def multiply():
+    numbers = request.json
+    return jsonify(int(numbers['first'] * numbers['second']))
+
+
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run(port=8080, host='0.0.0.0')
